bf59531784016b80bd959cef3fb1fc1e5adc6199
Travis Sheppard <tshepp@stanford.edu>
2017-03-23 16:10:19 -0700

    more basic reservation data

--- src/js_src/containers/reservation/basicReservationInfo.js
+++ src/js_src/containers/reservation/basicReservationInfo.js
@@ -16,6 +16,18 @@ class BasicReservationInfo extends Component {
             <input type='text' />
           </label>
         </div>
+        <div>
+          <label>
+            Description of gene name acronym
+            <input type='text' />
+          </label>
+        </div>
+         <div>
+          <label>
+            Notes about the gene
+            <input type='text' />
+          </label>
+        </div>
         <div className='text-right'>
           <a className='button disabled'>Next <i className='fa fa-chevron-right' /></a>
         </div>
--- src/js_src/containers/reservation/new.js
+++ src/js_src/containers/reservation/new.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 
 import ProgressTracker from '../../components/progressTracker';
 // import ReferenceForm from '../../components/referenceForm';
-// import BasicReservationInfo from './basicReservationInfo';
+import BasicReservationInfo from './basicReservationInfo';
 // import BasicColleagueInfo from './basicColleagueInfo';
-import GeneNameReservationSummary from '../../components/geneNameReservationSummary';
+// import GeneNameReservationSummary from '../../components/geneNameReservationSummary';
 
 class NewGeneNameReservation extends Component {
   render() {
     return (
       <div>
         <h1>Reserve a Gene Name</h1>
-        <ProgressTracker currentStep={3} steps={STEPS} />
-        <GeneNameReservationSummary />
+        <ProgressTracker currentStep={0} steps={STEPS} />
+        <BasicReservationInfo />
       </div>
     );
   }
